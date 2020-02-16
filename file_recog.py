8512c140c90beee5d179e2e3a6133c2d677f1efd
lucmi22py <lucmi.ggg@gmail.com>
2020-02-16 10:37:47 +0900

    test file on iPad

--- file_recog.py
+++ file_recog.py
@@ -1,4 +1,9 @@
-print('回数を入力してください')
-num = int(input())
-print(num*2)
-print(type(num))
+file = open('フルパス¥¥ファイル名.kfk', 'r')
+line = file.readline()
+while line:
+	if line in str('評価値'):
+		print('存在します:' + str(line))
+	else:
+		print('存在しません')
+file.close()
+print('処理を終了しました')
