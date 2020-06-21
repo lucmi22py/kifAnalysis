254578af8a3f0c97d1ecc3377ad8167a1ceec208
lucmi22py <lucmi.ggg@gmail.com>
2020-06-21 23:29:01 +0900

    中変更:dl_kif_class修正(def kifana()追加)

--- dl_kif_class.py
+++ dl_kif_class.py
@@ -6,6 +6,12 @@ from selenium.common.exceptions import NoSuchElementException
 import pyperclip
 import re
 import os
+import glob
+import pathlib
+
+import kif_handle
+import kfkfilemove
+import kfkfiling
 import Game_per_Day
 
 
@@ -60,21 +66,63 @@ class DLKifClass:
             file_kif.write(pre_paste)
             file_kif.close()
             print("ファイル作成完了。title: " + title_kif)
-
-            '''
-            css セレクタを使う場合
-            elem = browser.find_element_by_css_selector("tr:nth-child(1) > td > div > div > div.dropdown-trigger > button.button.arrow_icon.is-small") # cssselector = 'tr:nth-child(i) > td > div > div > div.dropdown-trigger > button.button.arrow_icon.is-small' 時々チェックすること! 2020/02/10まで
-            elem2 = browser.find_element_by_css_selector("tr:nth-of-type(1) td > div> div> div> div> div.is-paddingless.has-link")
-
-            〈issues〉
-            例外処理…xpath変更差分が取れるようにする。
-            '''
         print("すべての処理は終了しました")
         browser.quit()
 
     def kifana(self):
-        t = 1
-        return t
+        print('回数を入力してください')
+        loop_num = int(input())
+        for num in range(1, loop_num + 1):
+            if __name__ == '__main__':
+                kif_handle.openkif()
+
+            if __name__ == '__main__':
+                kif_handle.anakif()
+
+            if __name__ == '__main__':
+                kif_handle.finana()
+
+            if __name__ == '__main__':
+                kif_handle.savekif()
+
+            # 同名kifファイル移動
+            path = 'C:\\Users\\Ryota Okunishi\\OneDrive\\棋譜\\shogiwarskifu\\rawkifu'
+            A1 = glob.glob(os.path.join(path, '*_ana.*'))  # 対象1
+            A1_list_str = A1[0]
+            B = pathlib.Path(A1_list_str)
+            Target1_deana = B.stem.rstrip("_ana")
+            A2 = glob.glob('C:\\Users\\Ryota Okunishi\\OneDrive\\棋譜\\shogiwarskifu\\rawkifu\\*.kif')  # 対象2
+            for A2_element in A2:
+                if Target1_deana in A2_element:
+                    if __name__ == '__main__':
+                        kfkfilemove.move_glob(r'C:\Users\Ryota Okunishi\OneDrive\棋譜\shogiwarskifu\rawkifu2',
+                                              A2_element)
+                    break
+
+            # kfkfilingによるkfk化は最後にすること
+            # pyファイルのあるディレクトリへ対象ファイルを移動させるため
+            # *_ana.kifファイルのkfk化
+            f = glob.glob('C:\\Users\\Ryota Okunishi\\OneDrive\\棋譜\\shogiwarskifu\\rawkifu\\*_ana.kif')
+            try:
+                f1 = f[0]
+                if __name__ == '__main__':
+                    kfkfiling.change_suffix(f1, '.kif', '.kfk')
+                else:
+                    print("file is not exist")
+            except IndexError:
+                print("*_ana.kifファイルは存在しません")
+            except NameError:
+                print("*_ana.kifファイルが見つからないので、f1は存在しません")
+
+            # kfkファイル移動
+            try:
+                if __name__ == '__main__':
+                    kfkfilemove.move_glob('C:\\Users\\Ryota Okunishi\\OneDrive\\棋譜\\shogiwarskifu\\analysis',
+                                          'C:\\Users\\Ryota Okunishi\\pycharm_projects\\kifAnalysis\\*_ana.kfk')
+            except IndexError:
+                print("kfkファイルは存在しません")
+
+            print('棋譜解析完了しました。:' + str(int(num)) + '局目')
 
 
 dl = DLKifClass('luc22')
