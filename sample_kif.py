4713e5b2c2c7f41c265f9cd9d47925b961b5b78c
lucmi22py <lucmi.ggg@gmail.com>
2020-09-21 23:08:53 +0900

    chromeバージョンチェック

--- sample_kif.py
+++ sample_kif.py
@@ -5,11 +5,30 @@
 import dl_kif_class as dlkf
 import kif_db as kdb
 from webdriver_manager.utils import chrome_version
+from selenium import webdriver
+import datetime
 
 # -*- 処理部 -*-
 # 動作チェック
 print('以下の実行を行います [y/n]')
-print(chrome_version())
+
+# chromeとchromedriverのバージョンチェック
+chrome_ver = str(chrome_version())
+chromedriver_ver = str('85.0.418')
+if chromedriver_ver == chrome_ver:
+    ok = str('バージョンチェック問題なし')
+    print(ok)
+else:
+    ng = str('バージョンが違います')
+    print(ng)
+    dt_now = datetime.datetime.now()
+    path_w = r'C:\Users\Ryota Okunishi\OneDrive\棋譜\shogiwarskifu\errorlog.txt'
+    with open(path_w, mode='w') as f:
+        f.write('///バージョンチェック///' + '\n' +
+                'check date: \t\t' + str(dt_now) + '\n' +
+                'chrome version: \t' + chrome_ver + '\n' +
+                'chromedriver version: \t' + chromedriver_ver + '\n' +
+                'result: \t\t' + ng + '\n')
 
 # 自身がコールする関数を調べる
 try:
